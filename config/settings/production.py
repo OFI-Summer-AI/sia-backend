c055d49b66325a17750a68c5d9c46a85ec4ded6f
Diomer-Sandoval <d.sandoval@ofiservices.com>
2026-05-20 10:27:31 -0500

    fix: add stdout logging for Railway and disable SSL redirect

--- config/settings/production.py
+++ config/settings/production.py
@@ -16,3 +16,29 @@ X_FRAME_OPTIONS = 'DENY'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
